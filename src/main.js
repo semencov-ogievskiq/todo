9e3910a7858e6843ee507d5ae84ebb9df453f0e9
Alex <semencov_ogievskiq@mail.ru>
2021-08-02 19:22:39 +0300

    feat: Добавить плагин Auto Routing

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify'
 import store from './store'
-import router from './router'
+import router from './plugins/router'
 
 Vue.config.productionTip = false
 
