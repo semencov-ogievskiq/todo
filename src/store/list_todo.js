305c9a0d854c2720d31a4a63599604ec968f74d8
amso <semencov_ogievskiq@mail.ru>
2021-08-09 12:06:40 +0300

    feat: Создать страницу "список дел"
    Для свзяи с сервером добавил зависимость axios
    Состояния фильтрации списка добавленно в глобальное состояние и sessionStorage

---
+++ src/store/list_todo.js
@@ -0,0 +1,52 @@
+let defaultState= {
+    options: {
+        page: 0,
+        itemsPerPage: 0,
+        sortBy: [],
+        sortDesc: [],
+        groupBy: [],
+        groupDesc: [],
+        multiSort: false,
+        mustSort: false
+    },
+    filters: {
+        search: ''
+    }
+}
+
+let options = sessionStorage.getItem('list_todo_options')
+let filters = sessionStorage.getItem('list_todo_filters')
+
+options = ( options )? JSON.parse(options) : defaultState.options
+filters = ( filters )? JSON.parse(filters) : defaultState.filters
+
+export default {
+    namespaced: true,
+    state: ()=>({ 
+        ...defaultState,
+        options: options,
+        filters: filters
+    }),
+    mutations: {
+        mutOptions(state, options){
+            state.options = options
+        },
+        mutFilters(state, filters){
+            state.filters = filters
+        }
+    },
+    actions: {
+        setOptions({commit},options){
+            sessionStorage.setItem('list_todo_options', JSON.stringify(options))
+            commit('mutOptions', {...options})
+        },
+        setFilters({commit}, filters){
+            console.log(defaultState.filters)
+            sessionStorage.setItem('list_todo_filters', JSON.stringify(filters))
+            commit('mutFilters', {...filters})
+        },
+        resetFilters({dispatch}){
+            dispatch('setFilters', defaultState.filters)
+        }
+    }
+}
